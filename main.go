d970ad2517cc31a10cb4dd8eea9b4827d8c879c7
deakR <antennacloud@outlook.com>
2026-04-21 22:46:14 +0530

    i have done a refactor of the same as before with channel and goroutine

--- main.go
+++ main.go
@@ -5,7 +5,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"io"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i]) //add the content upto \n
+				data = data[i+1:] //add the remaining content from that 8 bytes after the \n (this will be added to the string after this if condition block)
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+
+	} ()
+
+	return out
+}
 
 func main() {
 	f, err := os.Open("messages.txt")
@@ -13,27 +50,11 @@ func main() {
 		log.Fatal("error", "error", nil)
 	}
 
-	str := ""
+	lines := getLinesChannel(f)
 
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		// fmt.Printf("read: %s\n", string(data[:n]))
-
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i]) //add the content upto \n
-			data = data[i+1:] //add the remaining content from that 8 bytes after the \n (this will be added to the string after this if condition block)
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-
-		str += string(data)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
-	}
+	
+	
 }
